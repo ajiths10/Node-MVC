79e797427784eec53ae71aa76255d1e784fa01b8
ajiths10 <ajithsnair10@gmail.com>
2022-05-19 21:18:47 +0530

    Dynamic routes

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,14 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) =>{
+  const proId = req.params.productId;
+  Product.findById(proId, product =>{
+    console.log(product);
+  } )
+  res.redirect('/');
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
