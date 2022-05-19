166317328e1c1931d1ed74759cdf58f10df5c1f7
ajiths10 <ajithsnair10@gmail.com>
2022-05-19 20:51:50 +0530

    new snapshot

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -10,16 +12,13 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const unknownPage = require('./routes/404Page')
-const contactus = require('./routes/contactus')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(contactus)
 
-app.use(unknownPage);
+app.use(errorController.get404);
 
 app.listen(3000);
