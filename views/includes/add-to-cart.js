b74bb10e063a1aaf95120a34dbbbacb9225e20e2
ajiths10 <ajithsnair10@gmail.com>
2022-05-22 00:41:58 +0530

    add size

--- views/includes/add-to-cart.js
+++ views/includes/add-to-cart.js
@@ -1,4 +1,5 @@
 <form action="/cart" method="post">
+<input type="hidden" name="productSize" value="<%= product.size %>">
 <button class="btn" type="submit">Add to Cart</button>
 <input type="hidden" name="productId" value="<%= product.id %>">
 </form>
