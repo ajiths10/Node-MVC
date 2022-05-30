8fe7917097f87adea6fe95293da091789e579e2b
ajiths10 <ajithsnair10@gmail.com>
2022-05-30 22:32:28 +0530

    SQL db create using js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const Sequelize = require('sequelize')
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize= require('./util/database');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,4 +21,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync().then(result => {
+    //console.log(result);
+    app.listen(3000);
+} )
+.catch(err => {
+    console.log(err);
+})
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host : 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: '12345678'
+const sequelize = new Sequelize('node-complete', 'root', '12345678', {
+    dialect: 'mysql', 
+    host: 'localhost'
 })
 
-module.exports = pool.promise();
+module.exports = sequelize;
