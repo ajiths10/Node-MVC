6bd33b3a714dccf82ca1914a142dd1df8587082b
ajiths10 <ajithsnair10@gmail.com>
2022-06-22 22:44:26 +0530

    increase quantity

--- controllers/shop.js
+++ controllers/shop.js
@@ -91,6 +91,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
     .getCart()
     .then((cart) => {
@@ -102,21 +103,21 @@ exports.postCart = (req, res, next) => {
       if (products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
       if (product) {
-        //...
-        //increment product count
+        console.log("====>", product);
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
-      return Product.findByPk(prodId)
-        .then((product) => {
-          return fetchedCart.addProduct(product, {
-            through: { quantity: newQuantity },
-          });
-        })
-        .catch((err) => console.log(err));
+      return Product.findByPk(prodId);
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
     })
     .then(() => {
-      //res.redirect("/cart")
+      res.redirect("/cart");
     })
     .catch((err) => console.log(err));
 };
