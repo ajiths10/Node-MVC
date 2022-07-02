cb3dd2e6a8dc2098632a14ba44adb0d92bc6ca5e
ajiths10 <ajithsnair10@gmail.com>
2022-07-02 22:35:35 +0530

    ready up backend

--- util/database.js
+++ util/database.js
@@ -1,8 +1,12 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
 
-const sequelize = new Sequelize('node-complete', 'root', '12345678', {
+//to get config vars
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   dialect: 'mysql',
-  host: 'localhost'
+  host: process.env.DB_HOST
 });
 
 module.exports = sequelize;
